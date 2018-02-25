30472c4ee905904c3045ed68cc0cd93c3fe0fe0e
Tien Dung <kidbandesit@gmail.com>
2018-02-25 10:28:06 +0700

    chech out

---
+++ LeaveManagement/Controllers/RoleController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LeaveManagement.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+
+namespace LeaveManagement.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+
+        #region Ctor
+
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        #endregion
+
+        #region Action Methods
+
+        /// <summary>
+        /// Get All Roles
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            //if (User.Identity.IsAuthenticated)
+            //{
+            //	if (!IsAdminUser())
+            //	{
+            //		return RedirectToAction("Index", "Home");
+            //	}
+            //}
+            //else
+            //{
+            //	return RedirectToAction("Index", "Home");
+            //}
+
+            var model = context.Roles.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private Boolean IsAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ LeaveManagement/Startup.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using LeaveManagement.Models;
+using Owin;
+using System.Security.Claims;
+
+[assembly: OwinStartupAttribute(typeof(LeaveManagement.Startup))]
+namespace LeaveManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRolesandUsers();
+		}
+
+
+		/// <summary>
+        /// Create default role "Admin", "Manager", "User" and admin account 
+        /// </summary>
+		private void CreateRolesandUsers()
+		{
+			ApplicationDbContext context = new ApplicationDbContext();
+
+			var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+			var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+			// creating first Admin Role and creating a default Admin User 
+			if (!roleManager.RoleExists("Admin"))
+			{
+
+                // Create admin role
+                var role = new IdentityRole
+                {
+                    Name = "Admin"
+                };
+                roleManager.Create(role);
+
+                // Admin is a super user who will maintain the website				
+
+                var user = new ApplicationUser
+                {
+                    UserName = "TienDung",
+                    Email = "tiendung@gmail.com"
+                };
+
+                var userPw = "Dung@123";
+
+				var chkUser = UserManager.Create(user, userPw);
+
+				//Add default User to Role Admin
+				if (chkUser.Succeeded)
+				{
+					var result = UserManager.AddToRole(user.Id, "Admin");
+				}
+			}
+
+			// creating Creating Manager role 
+			if (!roleManager.RoleExists("Manager"))
+			{
+                var role = new IdentityRole
+                {
+                    Name = "Manager"
+                };
+                roleManager.Create(role);
+
+			}
+
+			// creating Creating Employee role 
+			if (!roleManager.RoleExists("Employee"))
+			{
+                var role = new IdentityRole
+                {
+                    Name = "Employee"
+                };
+                roleManager.Create(role);
+
+			}
+		}
+	}
+}
